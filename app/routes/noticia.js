df53db90bf35f15579e9fb825ca4b259e4755e1c
Márcio Passos <marciopassosbel@gmail.com>
2016-09-23 00:38:01 -0300

    clear store...

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	slug:'',
 	model(params) {
 		let id = params.slug;
+		this.set('slug', id);
 		// console.log('route: ', id);
 		// let postContent = this.store.recordIsLoaded('post', id);
 		// console.log(postContent);
@@ -13,4 +15,22 @@ export default Ember.Route.extend({
 		// 	return this.store.peekRecord('post', id);
 		// }
 	}
+	// ,
+	// actions: {
+	// 	willTransition() {
+	// 		console.log('gone off single');
+	// 		// let slugId = this.get('slug');
+	// 		// let st = this.store;
+	// 		// st.findRecord('post', slugId).then(function(post) {
+	// 		// 	let postId = post.get('id');
+	// 		// 	console.log(post.get('title'));
+	// 		// 	console.log(post.get('slug'));
+	// 		// 	if (st.recordIsLoaded('post', postId)) {
+	// 		// 		console.log('unloaded...');
+	// 		// 		st.unloadRecord(post);
+	// 		// 	}
+	// 		// });
+	// 		// this.store.unloadAll('post');
+	// 	}	
+	// }
 });
--- app/routes/tag.js
+++ app/routes/tag.js
@@ -3,11 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	slug: '',
 	model(params) {
-		let tagId = params.tagid;//Ember.get(this.modelFor('tag'), 'tagid');
-		// console.log('opa: ', tagId);
+		let tagId = params.tagid;
 		this.set('slug', tagId);
-		// se nao tiver listados os posts, pegar via get
-		// caso ja tenha ido em notícias, pegar do store
 		return this.store.query('post', {
 			tagId
 		});
@@ -15,5 +12,19 @@ export default Ember.Route.extend({
 	setupController(controller) {
 		this._super(...arguments);
 		controller.set('slug', this.get('slug'));
+	},
+	actions: {
+		willTransition() {
+			console.log('gone off');
+			// debugger;
+			// this.controller.get('model').content.map(post => {
+			// 	console.log(post.id);
+			// 	if (this.store.recordIsLoaded('post', post.id)) {
+			// 		console.log('apagando ' + post.id);
+			// 		this.store.unloadRecord(post);
+			// 	}
+			// });
+			this.store.unloadAll('post');
+		}	
 	}
 });
